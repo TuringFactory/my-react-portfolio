8aa048eb965b919eebcc08369cdc1edb4e5c9036
TuringFactory <nguyenpauly1@gmail.com>
2023-02-15 12:03:07 -0500

    Updates

---
+++ precache-manifest.772bf0ad8efa7073dbfac348fa5bde0d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "073604d6de31abe62c8e",
+    "url": "/my-react-portfolio/static/css/main.8c46dec4.chunk.css"
+  },
+  {
+    "revision": "073604d6de31abe62c8e",
+    "url": "/my-react-portfolio/static/js/main.073604d6.chunk.js"
+  },
+  {
+    "revision": "48fc70a51432ec3cd299",
+    "url": "/my-react-portfolio/static/js/1.48fc70a5.chunk.js"
+  },
+  {
+    "revision": "306dca2831287f39777c",
+    "url": "/my-react-portfolio/static/css/2.4ca982b9.chunk.css"
+  },
+  {
+    "revision": "306dca2831287f39777c",
+    "url": "/my-react-portfolio/static/js/2.306dca28.chunk.js"
+  },
+  {
+    "revision": "58bb861f719127bd4e9b",
+    "url": "/my-react-portfolio/static/js/runtime~main.58bb861f.js"
+  },
+  {
+    "revision": "1d90fdb9b844bdfc57b78cbdbce8f0fb",
+    "url": "/my-react-portfolio/static/media/profile.1d90fdb9.jpg"
+  },
+  {
+    "revision": "c3f99d265fe5585c2b845cdfb480aa17",
+    "url": "/my-react-portfolio/static/media/lockheed-martin-logo.c3f99d26.png"
+  },
+  {
+    "revision": "14e3edb5d56a2f926fdf1e12e761dd6b",
+    "url": "/my-react-portfolio/static/media/PaulNguyenResume2022.14e3edb5.pdf"
+  },
+  {
+    "revision": "20dc0e5324ba0d1786e6b2a9c7d307e2",
+    "url": "/my-react-portfolio/static/media/vcu.20dc0e53.png"
+  },
+  {
+    "revision": "1b1be853139c2f132975000b86feb302",
+    "url": "/my-react-portfolio/static/media/reynolds.1b1be853.png"
+  },
+  {
+    "revision": "65afde230ad929f776e7dcbfc53c7d30",
+    "url": "/my-react-portfolio/index.html"
+  }
+];
