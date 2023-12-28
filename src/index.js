034df6bdc60595fbafecdb65e8640388ead4e945
TuringFactory <nguyenpauly1@gmail.com>
2023-12-28 13:19:11 -0500

    AWS amplify setup

--- src/index.js
+++ src/index.js
@@ -1,41 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 import {
   createBrowserRouter,
   RouterProvider,
-  createHashRouter
+  createHashRouter,
 } from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
-import 'bootstrap/dist/css/bootstrap.css';
-import WorkExperience from './components/workexperience';
-import MyNavbar from './components/navbar';
-import EducationPage from './components/educationPage';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import "bootstrap/dist/css/bootstrap.css";
+import WorkExperience from "./components/workexperience";
+import MyNavbar from "./components/navbar";
+import EducationPage from "./components/educationPage";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-document.body.style.background = '#f3e0dc'
+const root = ReactDOM.createRoot(document.getElementById("root"));
+document.body.style.background = "#f3e0dc";
 
-const router = createHashRouter([{
-  path:"/",
-  element: <App></App>
-},
-{
-  path:"/workexperience",
-  element: <WorkExperience></WorkExperience>
-},
-{
-  path:"/education",
-  element:<EducationPage></EducationPage>
-}
-])
+const router = createHashRouter([
+  {
+    path: "/",
+    element: <App></App>,
+  },
+  {
+    path: "/workexperience",
+    element: <WorkExperience></WorkExperience>,
+  },
+  {
+    path: "/education",
+    element: <EducationPage></EducationPage>,
+  },
+]);
 
 root.render(
   <React.Fragment>
     <MyNavbar></MyNavbar>
-    <RouterProvider router = {router}/>
+    <RouterProvider router={router} />
   </React.Fragment>
 );
 
